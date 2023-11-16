ada7fd40616523fd3d69956712f1451bf3ea862e
Jhon-D15 <149914828+Jhon-D15@users.noreply.github.com>
2023-11-16 13:00:31 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 
-public class Main {
+public class ejercicio2 {
     public static void main(String[] args) {
+
         Scanner lector = new Scanner(System.in);
 
-        int base;
-        int altura;
-        int area;
+        double numero1;
+        double numero2;
+        double resultado;
 
-        System.out.println("ingrese el valor de la base");
-        base= lector.nextInt();
+        System.out.println("ingrese un numero");
+        numero1= lector.nextDouble();
 
-        System.out.println("ingrese el valor de la altura");
-        altura= lector.nextInt();
+        System.out.println("ingrese otro numero");
+        numero2= lector.nextDouble();
 
-        area = (base * altura)/2;
+        resultado = (numero1 + numero2);
 
-        System.out.println("el area del triangulo es:"+" "+area);
-
-
+        System.out.println("la suma de los dos numero es:"+" "+resultado);
 
     }
 }
